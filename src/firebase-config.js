ceafd7a8d0e9ece8d503d7b721247881c894282f
Anjali Vijay Kaware <anjalikaware6402@gmail.com>
2023-11-30 22:38:28 +0530

    firebase authentication

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -18,20 +18,44 @@
 
 
 
-import firebase from 'firebase/compat/app'
- import 'firebase/compat/auth';
- import 'firebase/compat/database'
+// import firebase from 'firebase/compat/app'
+//  import 'firebase/compat/auth';
+//  import 'firebase/compat/database'
  
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBBOv9KNIVTsNwr_tSGAplO659p76iqIOA",
+//   authDomain: "food-waste-a0b20.firebaseapp.com",
+//   projectId: "food-waste-a0b20",
+//   storageBucket: "food-waste-a0b20.appspot.com",
+//   messagingSenderId: "349467816262",
+//   appId: "1:349467816262:web:b3205a8e603352cd5fb547",
+//   measurementId: "G-T52BKRR0RQ"
+// };
+
+// // Initialize Firebase
+// const app = firebase.initializeApp(firebaseConfig);
+// export default app /
+
+// Import the functions you need from the SDKs you need
+import firebase from 'firebase/compat/app'
+  import 'firebase/compat/auth';
+  import 'firebase/compat/database'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBBOv9KNIVTsNwr_tSGAplO659p76iqIOA",
-  authDomain: "food-waste-a0b20.firebaseapp.com",
-  projectId: "food-waste-a0b20",
-  storageBucket: "food-waste-a0b20.appspot.com",
-  messagingSenderId: "349467816262",
-  appId: "1:349467816262:web:b3205a8e603352cd5fb547",
-  measurementId: "G-T52BKRR0RQ"
+  apiKey: "AIzaSyCGbN_JenEuN-00jmXObXBLmZLcc0A51s0",
+  authDomain: "anjali-f467d.firebaseapp.com",
+  projectId: "anjali-f467d",
+  storageBucket: "anjali-f467d.appspot.com",
+  messagingSenderId: "494699198568",
+  appId: "1:494699198568:web:dc38bf606d0429074ca3dd",
+  measurementId: "G-0J3SCM7ZNG"
 };
 
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
-export default app 
+//const analytics = getAnalytics(app);
+export default app
